616fd846b7882ca89d69b240900736b4f976ba81
maksb2bb <114099845+maksb2bb@users.noreply.github.com>
2023-01-29 21:13:37 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const burger = document.querySelector('.burger')
+const container = document.querySelector('.container')
+const screens = document.querySelectorAll('.screen')
+
+burger.addEventListener('click', () =>{
+    container.classList.toggle('active')
+})
+
+function replaceBg(id) {
+    const bg = document.getElementById(id)
+    screens.forEach( screen => {
+        screen.style.display = 'none'
+    })
+    bg.style.display = 'block'
+}
+
+function changeBg() {
+    const links = document.querySelectorAll('.link')
+
+    links.forEach((link, index) =>{
+        link.addEventListener('click', e => {
+            e.preventDefault()
+            replaceBg(e.target.dataset.link)
+        })
+        container.classList.toggle('active')
+    })
+
+    screens.forEach(screen => {
+        screen.style.display = 'none'
+        screens[0].style.display = 'block'
+    })
+}
+changeBg()
